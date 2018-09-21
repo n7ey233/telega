988a6306d374ae3218e3b78ab4e2c34304541515
n7ey233 <n7ey233@gmail.com>
2018-09-21 15:23:43 +1100

    init

--- core/views.py
+++ core/views.py
@@ -247,7 +247,7 @@ def reply(method, q1 = None, q2 = None):
         dsa = product.objects.get(pk=method[1:])
         #proverka buyera
         if dsa.buyer == q1:
-            text = dsa.type_of_product.name + ' +mestopolojeniye i prochaya poebota, ya svoe delo sdelal, nuzhna informaciya o tom kakiye danniye budut o zakladke\n\n k primeru: geolokaciya, foto,  \n\n nuzhno babos, a to eto parojnyak'
+            text = dsa.type_of_product.name+'\n\nГеолокация: гугл яндекс 2гис?' + '\n\nДополнительное описание: '+dsa.commentary +'.\n\n Ссылка на фото: '+ dsa.foto_link+' +mestopolojeniye i prochaya poebota, ya svoe delo sdelal, nuzhna informaciya o tom kakiye danniye budut o zakladke\n\n k primeru: geolokaciya, foto,  \n\n nuzhno babos, a to eto parojnyak'
         else:
             text= 'К сожалению, данные об этом товаре принадлежат другому пользователю.'
             l1.append(inline_keyboard('Помощь', 'support'))
