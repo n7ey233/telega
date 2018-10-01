092d9f5090e5597a09c2798bdceaac6ef8a42e24
n7ey233 <n7ey233@gmail.com>
2018-10-01 19:24:53 +1100

    upd

--- core/views.py
+++ core/views.py
@@ -197,7 +197,7 @@ def reply(method, q1 = None, q2 = None):
             text+='\nУвы, с вашего аккаунта ещё не было покупок'
         else:
             for i in asdf:
-                l1.append(inline_keyboard(i.type_of_product.name, 'j'+str(i.pk)))
+                l1.append(inline_keyboard(i.type_of_product.name+' '+i.sold_date, 'j'+str(i.pk)))
         l1.append(inline_keyboard('На главную', '/privet'))
     #balance itd
     elif method == 'cashbalance':
